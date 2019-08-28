570559f96c7ffa3655d8c1beb38a7d131888feb1
Jeremy <JeremyPlott@gmail.com>
2019-08-28 15:57:21 -0400

    Lots of methods added to Account class

---
+++ AccountClass/Account.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountClass {
+
+    class Account{
+
+        private static int nextAccountNbr = 0;  //static means this one property is unique to the class, not created per instance
+
+        public int Id { get; private set; }
+        private decimal Balance { get; set; } = 0.0M; //sets default value. The M is to put as dec instead of double.
+        public string Description { get; set; }
+
+        public void Transfer(Account acct, decimal amount) {            
+            var withdrawSuccessful = this.Withdraw(amount); //this is a variable that referrs to an instance of the class get gets provided
+            if(withdrawSuccessful) { //if(!____) flips the boolean.
+                acct.Deposit(amount);
+            }
+            Console.WriteLine("Transfer failed.");
+        }
+
+        public Account() {
+            Id = ++nextAccountNbr;
+        }  //constructors are conventionally at the end of the class. Same name as class.
+
+        public Account(string Description) : this () { // : this () calls the other constructor first with () parameter.
+            this.Description = Description; //distinguishes between parameter and property of the same name
+        }
+
+        public decimal GetBalance() {
+            return Balance;
+        }
+
+        private bool CheckAmountIsPositive(decimal amount) {
+            if (amount < 0) {
+                Console.WriteLine("Amount cannot be negative.");
+                return false;
+            }
+            return true;
+        }
+
+        public bool Deposit(decimal Amount) { //these variables are only referenced within the specific method, even with the same name.
+            var valid = CheckAmountIsPositive(Amount);
+            if (valid == true) {
+                Balance += Amount;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Withdraw(decimal Amount) {
+            var valid = CheckAmountIsPositive(Amount);
+            if(valid == true) {     //avoid nested if statements beyond 3 levels
+                if (Amount > Balance) {
+                    Console.WriteLine("Insufficient funds.");
+                } else {
+                    Balance -= Amount;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- AccountClass/Program.cs
+++ AccountClass/Program.cs
@@ -1,11 +1,57 @@
 ﻿using System;
 
+/*
+ * Homework
+ * create customer class.
+ * unique int 'Id'
+ * string name of company
+ * string city
+ * string state
+ * decimal sales (default 0)
+ * bool active (default true)
+ * 
+ * Put in a default constructor, used or not
+ * provide a way to set all but sales + Id
+ * method for adding to sales
+ * auto-gen Id
+*/
+
 namespace AccountClass {
 
     class Program {
 
         static void Main(string[] args) {
-        //asdasda
+
+            var acct1 = new Account("Primary Checking");
+            //acct1.Balance = 1000000m; //should not be allowed to do this without calling a method. Safety nets.
+
+            Console.WriteLine($"{acct1.Description} account - {acct1.Id} has a balance of {acct1.GetBalance()}.");
+
+            acct1.Deposit(1000);
+            Console.WriteLine($"{acct1.Description} account - {acct1.Id} has a balance of {acct1.GetBalance()}.");
+
+            acct1.Withdraw(5000);
+            Console.WriteLine($"{acct1.Description} account - {acct1.Id} has a balance of {acct1.GetBalance()}.");
+
+            var balance = acct1.GetBalance();
+            Console.WriteLine(balance);
+
+            acct1.Withdraw(-1000000);
+            Console.WriteLine($"{acct1.Description} account - {acct1.Id} has a balance of {acct1.GetBalance()}.");
+
+
+            var acct2 = new Account("Secondary Checking");
+            Console.WriteLine($"{acct2.Description} account - {acct2.Id} has a balance of {acct2.GetBalance()}.");
+
+            acct1.Transfer(acct2, 2000);
+            Console.WriteLine($"{acct2.Description} account - {acct2.Id} has a balance of {acct2.GetBalance()}.");
+
+            acct2.Deposit(5);
+            Console.WriteLine($"{acct2.Description} account - {acct2.Id} has a balance of {acct2.GetBalance()}.");
+            Console.WriteLine($"{acct1.Description} account - {acct1.Id} has a balance of {acct1.GetBalance()}.");
+
         }
+
+
     }
 }
